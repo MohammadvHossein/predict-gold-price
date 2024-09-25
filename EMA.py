dfb4f0f6e624db7a4fcb5fed809cc802324d0f6c
MohammadvHossein <habibpour.programming@gmail.com>
2024-09-25 13:03:57 +0330

    add EMA and Ensemble and modify MACD

--- EMA.py
+++ EMA.py
@@ -5,13 +5,11 @@ data = pd.read_csv('evaluations/test_15min.csv')
 data["Date"] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
 
-data['EMA_12'] = data['Close'].ewm(span=2, adjust=False).mean()
+data['EMA_12'] = data['Close'].ewm(span=7, adjust=False).mean()
 data['EMA_26'] = data['Close'].ewm(span=26, adjust=False).mean()
-data['MACD'] = data['EMA_12'] - data['EMA_26']
-data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean()
 data['signal'] = 0
 
-data.loc[data['MACD'] > data['Signal_Line'], 'signal'] = 1
+data.loc[data['EMA_12'] > data['EMA_26'], 'signal'] = 1
 
 ema_data = data[["signal"]]
 
---
+++ ensemble.py
@@ -0,0 +1,31 @@
+from evaluations.evaluation import evaluation , true
+import pandas as pd
+
+data = pd.read_csv('evaluations/Train.csv')
+data["Date"] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
+
+data['EMA_5'] = data['Close'].ewm(span=5, adjust=False).mean()
+data['EMA_10'] = data['Close'].ewm(span=12, adjust=False).mean()
+data['EMA_12'] = data['Close'].ewm(span=12, adjust=False).mean()
+data['EMA_26'] = data['Close'].ewm(span=26, adjust=False).mean()
+
+data['MACD'] = data['EMA_12'] - data['EMA_26']
+data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean()
+
+data['signalE'] = -1
+
+data.loc[(data['EMA_5'] > data['EMA_10']) & (data['MACD'] > data['Signal_Line']), 'signalE'] = 1
+data.loc[(data['EMA_5'] < data['EMA_10']) & (data['MACD'] < data['Signal_Line']), 'signalE'] = 0
+
+ema_data = data[["signal","signalE","body"]]
+
+new_data = ema_data[ema_data['signalE'] >= 0]
+
+pred = ema_data[ema_data["signal"] == ema_data['signalE']]["body"].sum()
+
+# pred = new_data['body'].sum()
+
+print(pred)
+print(pred / true)
+
