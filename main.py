fd24bf3799dafaac0a2785a644d022ae02b618b6
MohammadvHossein <habibpour.programming@gmail.com>
2024-09-28 12:47:57 +0330

    add XG boost

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from naive_bayes import naive_bayes_classifier
+from XG_boost import XG_boost_classifier
 from evaluations import evaluation
 
+print("naive_bayes :")
 result = naive_bayes_classifier.classifier()
-print(evaluation.evaluation(result ,data='evaluations/test.csv'))
+print(evaluation.evaluation(result, data='evaluations/test.csv'), "\n")
 
+print("XG_boost :")
+result = XG_boost_classifier.classifier()
+print(evaluation.evaluation(result, data='evaluations/test.csv'), "\n")
